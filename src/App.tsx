97283f7c70d401f9cff380c3d27d35963d732e5e
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-11-02 18:58:17 -0300

    get coords and verify if user turn on geolocalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import { ContainerIfNotPermission } from './styles/pages/app';
+
+type CoordsType = {
+  latitude: number
+  longitude: number
+}
+
 function App() {
+  const [coords, setCoords] = useState < CoordsType|null >(null);
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCoords({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
   return (
     <div>
-      aaaaaaa
+      {coords === null ? (
+        <ContainerIfNotPermission>
+          <div className="content">
+            <h1>Turn on geolocalization for use this web app</h1>
+            <div className="load"><div /></div>
+          </div>
+        </ContainerIfNotPermission>
+      ) : (
+        <div>aaaaaaaaaaaaaa</div>
+      )}
     </div>
   );
 }
