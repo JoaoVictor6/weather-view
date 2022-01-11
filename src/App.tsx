f4d1639899c311b98dd2cc5fe01272a1f519d69b
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-01-11 19:28:01 -0300

    configure header for permission denied

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
+import { useEffect, useState } from 'react';
 import Menu from './components/Menu';
 import { Container } from './styles/pages/app';
 import sunrise from './assets/images/sunrise.png';
 import sunset from './assets/images/sunset.png';
+import { getWeather } from './utils/axios';
+import { APIResponse, ForecastsProps } from './interfaces';
+import useGeolocation from './hooks/useGeolocation';
+
+import locationNotFound from './assets/images/location-not-found.svg';
 
 function App() {
+  const [forecasts, setForecasts] = useState<ForecastsProps[]>();
+  const [weatherInfo, setWeatherInfo] = useState<
+    Pick<APIResponse, 'current_observation' | 'location'>
+  >();
+
+  const { coords } = useGeolocation();
+  useEffect(() => {
+    (async () => {
+      const response = await getWeather(coords);
+      if (response !== undefined) {
+        setForecasts(response.forecasts);
+        setWeatherInfo({
+          current_observation: response.current_observation,
+          location: response.location,
+        });
+      }
+    })();
+    console.log(forecasts);
+  }, [coords]);
   return (
     <>
-      <Menu />
+      <Menu title={{
+        city: weatherInfo?.location.city,
+        region: weatherInfo?.location.region,
+      }}
+      />
       <Container>
         <header>
           <section className="header__title">
-            <h1>20 º</h1>
+            {weatherInfo === undefined ? (
+              <img src={locationNotFound} alt="Location not found" />
+            ) : (
+              <h1>
+                {weatherInfo?.current_observation.condition.temperature}
+                {' '}
+                º
+              </h1>
+            )}
           </section>
           <section className="header__footer">
-            <p>
-              <img src={sunrise} alt="sunrise hour" />
-              7:23 am
-            </p>
-            <p>
-              5:10 pm
-              <img src={sunset} alt="sunset hour" />
-            </p>
+            {weatherInfo === undefined ? (
+              <p>Not found</p>
+            ) : (
+              <>
+                <p>
+                  <img src={sunrise} alt="sunrise hour" />
+                  {weatherInfo?.current_observation.astronomy.sunrise}
+                </p>
+                <p>
+                  {weatherInfo?.current_observation.astronomy.sunset}
+                  <img src={sunset} alt="sunset hour" />
+                </p>
+              </>
+            )}
           </section>
         </header>
       </Container>
