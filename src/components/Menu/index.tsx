168358e57ba8f470619e74e655fbbfaec29221d6
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-01-14 13:33:56 -0300

    apply api

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -31,7 +31,12 @@ export default function Menu({ title }: MenuProps) {
           <path fill="#fff" fillRule="evenodd" d="M11.2 5.6a5.6 5.6 0 100 11.2 5.6 5.6 0 000-11.2zm-8.4 5.6a8.4 8.4 0 1115.246 4.866l6.744 6.744a1.4 1.4 0 01-1.98 1.98l-6.742-6.743A8.4 8.4 0 012.8 11.2z" clipRule="evenodd" />
         </svg>
       </button>
-      <SearchAddress appear={search} />
+      <SearchAddress
+        onSearch={() => {
+          setSearch(false);
+        }}
+        appear={search}
+      />
     </Nav>
   );
 }
--- src/components/SearchAddress/index.tsx
+++ src/components/SearchAddress/index.tsx
@@ -1,18 +1,26 @@
 /* eslint-disable consistent-return */
-import { useEffect, useRef } from 'react';
+import { useContext, useEffect, useRef } from 'react';
+import { WeatherContext } from '../../App';
+import { getLocalization, getWeather } from '../../utils/axios';
 import { appearAnimation, disappearAnimation, Form } from './style';
 
 interface SearchAddressProps {
   appear: boolean
+  // eslint-disable-next-line react/require-default-props
+  onSearch?: () => void
 }
 
-export default function SearchAddress({ appear }:SearchAddressProps) {
+export default function SearchAddress({ appear, onSearch }:SearchAddressProps) {
   const formRef = useRef<HTMLFormElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const weatherContext = useContext(WeatherContext);
+
   useEffect(() => {
     if (appear) {
       formRef.current?.classList.add('flex');
       formRef.current?.classList.remove('disappear');
       formRef.current?.classList.add('appear');
+      inputRef.current?.focus();
     } else {
       formRef.current?.classList.remove('appear');
       formRef.current?.classList.add('disappear');
@@ -34,14 +42,23 @@ export default function SearchAddress({ appear }:SearchAddressProps) {
     return () => formRef.current?.removeEventListener('animationend', () => {});
   }, [appear]);
 
+  async function weatherUpdate() {
+    const { latitude, longitude } = await getLocalization(inputRef.current?.value as string);
+    const response = await getWeather({ latitude, longitude });
+    weatherContext?.setWeatherInfo(response);
+    weatherContext?.setForecasts(response?.forecasts);
+  }
   return (
     <Form
       ref={formRef}
       onSubmit={(e) => {
         e.preventDefault();
+        weatherUpdate();
+        // eslint-disable-next-line no-unused-expressions
+        onSearch && onSearch();
       }}
     >
-      <input type="text" placeholder="Search for any address" />
+      <input ref={inputRef} type="text" placeholder="Search for any address" />
       <button type="button">
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22">
           <path fill="#fff" fillRule="evenodd" d="M5.555 4.455a7.7 7.7 0 1110.89 10.89L11 20.79l-5.445-5.445a7.7 7.7 0 010-10.89zM11 12.1a2.2 2.2 0 100-4.4 2.2 2.2 0 000 4.4z" clipRule="evenodd" />
